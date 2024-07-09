ab83901022de044cc854870ecfe66de03ada179b
Georgio01 <G.hassrouty@bsynchro.com>
2024-07-09 22:37:57 +0300

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { serverStatusComponent } from './dashboard/server-status.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,serverStatusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'ticket_project';
+
+  dummyTrafficData=[
+   { id:'d1',
+    value:433,
+  },
+
+  {
+    id:'d2',
+    value:260,
+  },
+  {id:'d3',
+    value:290,
+  },
+
+  {
+    id:'d4',
+    value:410,
+  },
+  {
+    id:'d5',
+    value:397,
+  },
+
+  {id:'d6',
+    value:488,
+  },
+
+  {
+    id:'d47',
+    value:589,
+  }
+
+];
+maxTraffic=Math.max(...this.dummyTrafficData.map((data)=>data.value));
 }
